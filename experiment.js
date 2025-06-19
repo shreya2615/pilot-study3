d1f4508b7618cff70914a5c9a6e5a84ce9ae6ded
shreya2615 <shreya.sharma2615@gmail.com>
2025-06-19 10:24:17 -0400

    Update experiment.js

--- experiment.js
+++ experiment.js
@@ -14,12 +14,14 @@ const jsPsych = initJsPsych({
 });
 
 const group = jsPsych.randomization.sampleWithoutReplacement(["male", "female"], 1)[0];
-const blockMap = {
-  a: Array.from({ length: 10 }, (_, i) => (i + 1).toString().padStart(2, "0")),
-  b: Array.from({ length: 10 }, (_, i) => (i + 11).toString().padStart(2, "0")),
-  c: Array.from({ length: 10 }, (_, i) => (i + 21).toString().padStart(2, "0"))
-};
-const blockOrder = ["a", "b", "c"];
+
+const imageAudioFlow = [
+  { images: [1, 2], audios: [1, 2, 3, 4] },
+  { images: [3, 4], audios: [5, 6, 7, 8] },
+  { images: [5, 6], audios: [9, 10, 11, 12] },
+  { images: [7, 8], audios: [13, 14, 15, 16] },
+  { images: [9, 10], audios: [17, 18, 19, 20] }
+];
 
 const makeSlider = (stimulusType, stimulusPath, question, min, max, step, labels) => {
   let stimulusHTML = stimulusType === "image"
@@ -30,7 +32,7 @@ const makeSlider = (stimulusType, stimulusPath, question, min, max, step, labels
 
   return {
     type: jsPsychSurveyHtmlForm,
-    preamble: `${stimulusHTML}<p><b>${question}</b></p><p><i>Please use the slider to make your selection.</i></p>`,
+    preamble: `${stimulusHTML}<p><i>Please use the slider to make your selection.</i></p>`,
     html: `
       <input type='range' name='response' min='${min}' max='${max}' step='${step}' style='width: 100%;'><br>
       <div style='display: flex; justify-content: space-between;'>
@@ -65,7 +67,7 @@ const consent = {
 const instructions = {
   type: jsPsychHtmlKeyboardResponse,
   stimulus: `
-    <p>You will be shown faces and voices in random order.</p>
+    <p>You will be shown faces and voices in alternating order.</p>
     <p>After each one, answer four questions about your impression using a slider.</p>
     <p>Press SPACE to begin.</p>
   `,
@@ -74,42 +76,31 @@ const instructions = {
 
 let timeline = [consent, instructions];
 
-let imageTrials = [];
-let audioTrials = [];
-
-blockOrder.forEach(blockKey => {
-  const ids = blockMap[blockKey];
-
-  ids.forEach(id => {
-    // Push 6 image variants × 4 questions each
+imageAudioFlow.forEach(groupSet => {
+  groupSet.images.forEach(imgID => {
     for (let v = 1; v <= 6; v++) {
-      const img = `all_images/${group}_face${id}_${v}.png`;
-      imageTrials.push(
-        makeSlider("image", img, "How dominant do you think this person is?", 1, 7, 1),
-        makeSlider("image", img, "How trustworthy do you think this person is?", 1, 7, 1),
-        makeSlider("image", img, "How honest do you think this person is?", 1, 7, 1),
-        makeSlider("image", img, "How tall do you think this person is?", 1, 13, 1)
+      const facePath = `all_images/${group}_face${imgID.toString().padStart(2, "0")}_${v}.png`;
+      timeline.push(
+        makeSlider("image", facePath, "How dominant do you think this person is?", 1, 7, 1),
+        makeSlider("image", facePath, "How trustworthy do you think this person is?", 1, 7, 1),
+        makeSlider("image", facePath, "How honest do you think this person is?", 1, 7, 1),
+        makeSlider("image", facePath, "How tall do you think this person is?", 1, 13, 1)
       );
     }
+  });
 
-    // Push 3 audio variants × 4 questions each
+  groupSet.audios.forEach(audioID => {
     for (let p = 1; p <= 3; p++) {
-      const audio = `all_audios/${group}_voice${id}_pitch${p}.wav`;
-      audioTrials.push(
-        makeSlider("audio", audio, "How dominant do you think this person is?", 1, 7, 1),
-        makeSlider("audio", audio, "How trustworthy do you think this person is?", 1, 7, 1),
-        makeSlider("audio", audio, "How honest do you think this person is?", 1, 7, 1),
-        makeSlider("audio", audio, "How tall do you think this person is?", 1, 13, 1)
+      const audioPath = `all_audios/${group}_voice${audioID.toString().padStart(2, "0")}_pitch${p}.wav`;
+      timeline.push(
+        makeSlider("audio", audioPath, "How dominant do you think this person is?", 1, 7, 1),
+        makeSlider("audio", audioPath, "How trustworthy do you think this person is?", 1, 7, 1),
+        makeSlider("audio", audioPath, "How honest do you think this person is?", 1, 7, 1),
+        makeSlider("audio", audioPath, "How tall do you think this person is?", 1, 13, 1)
       );
     }
   });
 });
-
-// Interleave: 10 images then 10 audios, repeat
-while (imageTrials.length || audioTrials.length) {
-  if (imageTrials.length) timeline.push(...imageTrials.splice(0, 40));
-  if (audioTrials.length) timeline.push(...audioTrials.splice(0, 40));
-}
 
 timeline.push({
   type: jsPsychHtmlKeyboardResponse,
